6f988b328fdd1f2d3891d99b2d06f7bc318d9f1f
FlorentLem <florentlempro@gmail.com>
2022-10-25 22:08:32 +0200

    feat(ui): added switch to change display type for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 
+import { Button, Input, Stack, Switch, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 import Card from "./Card";
@@ -10,6 +11,7 @@ export default function App() {
   const [data, setData] = useState<Array<any>>([]);
   const [search, setSearch] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
+  const [isFlipping, setIsFlipping] = useState<boolean>(true);
 
   const searchCard = () => {
     setLoading(true);
@@ -46,12 +48,11 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <div
-        style={{
-          padding: "20px",
-        }}
-      >
-        <input
+      <Stack direction={"row"} spacing={4} align={"center"}>
+        <Input
+          color={"white"}
+          colorScheme={"blue"}
+          size={"sm"}
           style={{
             marginRight: "10px",
           }}
@@ -60,15 +61,47 @@ export default function App() {
           value={search || ""}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button onClick={searchCard}>Search</button>
-      </div>
+        <Button
+          color={"white"}
+          colorScheme={"gray"}
+          variant="outline"
+          size={"sm"}
+          onClick={searchCard}
+        >
+          Search
+        </Button>
+        <Switch
+          size={"lg"}
+          checked={isFlipping}
+          onChange={() => setIsFlipping(!isFlipping)}
+        />
+        <Text width={300} color={"white"}>
+          Switch display mode
+        </Text>
+      </Stack>
       <div className="App">
         {loading && <Loader />}
         {!loading &&
           data.length > 0 &&
-          data.map((pokemon) => (
-            <Card key={pokemon.id} src={pokemon.images.large} />
-          ))}
+          data.map((pokemon) => {
+            if (!isFlipping)
+              return <Card key={pokemon.id} src={pokemon.images.large} />;
+
+            return (
+              <img
+                key={pokemon.id}
+                src={pokemon.images.large}
+                alt="pokemon"
+                width="260"
+                height="365"
+                style={{
+                  margin: "10px",
+                  padding: "10px 10px 10px 10px",
+                  borderRadius: "20px",
+                }}
+              />
+            );
+          })}
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
